7b30d6a0e0b414d04b3649356274bb749ec59263
cclamazonia-cmd <cclamazonia@gmail.com>
2026-03-19 00:43:33 +0100

    Update blmf-supabase.js

--- js/blmf-supabase.js
+++ js/blmf-supabase.js
@@ -1,13 +1,13 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://uflwmikiyjfnikiphtcp.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmbHdraWtpampmbmlraXBodGNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NjM0MTksImV4cCI6MjA4NTUzOTQxOX0.x-qCVvWD2pfjpDEp7hifQ2wCKAzOsO3gaA7KNdzD60I";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmbHdtaWtpeWpmbmlraXBodGNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MzIyNDUsImV4cCI6MjA4OTQwODI0NX0.kCs7nPg08ofjb9CWwRH9xVN6BjanrAC5pj418line1o";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: window.sessionStorage,
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true
-  }
+    detectSessionInUrl: true,
+  },
 });
